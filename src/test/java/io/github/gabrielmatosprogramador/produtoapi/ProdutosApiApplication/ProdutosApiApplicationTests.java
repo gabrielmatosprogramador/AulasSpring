695de381c3bf9ac29b1105877a518a9c9b76c3ba
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-04-07 10:34:19 -0300

    Criação do projeto

---
+++ src/test/java/io/github/gabrielmatosprogramador/produtoapi/ProdutosApiApplication/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielmatosprogramador.produtoapi.ProdutosApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
